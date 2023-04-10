3201415e3a4aef68b3c0f55a36edeef165c51014
Oleksii Shulzhenko <oleksii.shulzhenko@zattoo.com>
2023-04-10 18:03:54 +0200

    Starting working ow dealing with first steps of the form

--- TerminBuchen.py
+++ TerminBuchen.py
@@ -1,11 +1,17 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, \
-    QHBoxLayout, QLineEdit, QPushButton, QLabel, QMessageBox
+    QHBoxLayout, QLineEdit, QPushButton, QLabel, QMessageBox, QComboBox
 from PyQt6 import QtGui
 from PyQt6.QtGui import QRegularExpressionValidator
 from PyQt6.QtCore import QRegularExpression
 import threading
 import sys
-from selenium import webdriver
+
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+
+import undetected_chromedriver as uc
+
 from time import sleep
 
 
@@ -22,11 +28,33 @@ class TerminBuchenModel:
         if self.driver is not None:
             self.driver.quit()
 
+# Open Chrome browser
+    # def open_web(self):
+    #     chromedriver = r".\chromedriver"
+    #     self.driver = webdriver.Chrome(chromedriver)
+    #     URL = self.controller.view.qle_url.text()
+    #     self.driver.get(URL)
+
+
+# Open Chrome browser
     def open_web(self):
-        chromedriver = r".\chromedriver"
-        self.driver = webdriver.Chrome(chromedriver)
-        URL = self.controller.view.qle_url.text()
+        self.driver = uc.Chrome()
+        URL = "https://otv.verwalt-berlin.de/ams/TerminBuchen/wizardng?sprachauswahl=de"
         self.driver.get(URL)
+
+# Additional steps in the funnel to generate string
+    def aggree_terms_cond(self):
+        sleep(int(10))
+        self.driver.find_element("id", "xi-cb-1").click()
+        self.driver.find_element("id", "applicationForm:managedForm:proceed").click()
+
+
+    def fill_fileds(self):
+        country = self.controller.view.fld_country.text()
+        self.driver.find_element("id", "xi-sel-400").select_by_visible_text(country)
+        self.driver.execute_script('document.getElementById("applicationForm:managedForm:proceed").click()')
+    
+
 
     def buchen(self):
         qle_delay = self.controller.view.qle_delay.text()
@@ -45,7 +73,7 @@ class TerminBuchenModel:
 
     def send_dgram(self):
         while self.keep_running:
-            state = self.buchen_event.isSet()
+            state = self.buchen_event.is_set()
             if state:
                 try:
                     self.driver.execute_script('document.getElementById("applicationForm:managedForm:proceed").click()')
@@ -72,6 +100,11 @@ class TerminBuchenView(QWidget):
         self.btn_buchen = None
         self.initUI()
 
+        # Defining fields for previous steps:
+        self.country = None
+        self.number_of_persons = None
+
+
     def closeEvent(self, event):
         self.controller.model.keep_running = False
 
@@ -83,6 +116,8 @@ class TerminBuchenView(QWidget):
         mainLayout = QVBoxLayout(self)
         hbox_r1 = QHBoxLayout()
         hbox_r2 = QHBoxLayout()
+        hbox_r3 = QHBoxLayout()
+
         ql_http = QLabel('Website', self)
         self.qle_url = QLineEdit(self)
         self.validator_url = QRegularExpressionValidator(self)
@@ -98,6 +133,11 @@ class TerminBuchenView(QWidget):
         self.btn_stop = QPushButton('Stop', self)
         ql_delay = QLabel('Delay in seconds', self)
         self.qle_delay = QLineEdit('30', self)
+
+        self.fld_country = QLineEdit('Ukraine', self)
+        self.fld_number_of_persons = QComboBox()
+        self.fld_number_of_persons.addItems(['eine Person', 'zwei Personen', 'drei Personen', 'vier Personen'])
+
         hbox_r1.addWidget(ql_http)
         hbox_r1.addWidget(self.qle_url)
         hbox_r1.addWidget(self.btn_open_web)
@@ -106,9 +146,14 @@ class TerminBuchenView(QWidget):
         hbox_r2.addWidget(self.btn_get_url)
         hbox_r2.addWidget(self.btn_buchen)
         hbox_r2.addWidget(self.btn_stop)
+        hbox_r3.addWidget(self.fld_country)
+        hbox_r3.addWidget(self.fld_number_of_persons)
+
+
         self.setLayout(mainLayout)
         mainLayout.addLayout(hbox_r1)
         mainLayout.addLayout(hbox_r2)
+        mainLayout.addLayout(hbox_r3)
 
     def center(self):
         qr = self.frameGeometry()
@@ -133,10 +178,16 @@ class TerminBuchenController:
         self.t.start()
 
     def on_btn_open_web_clicked(self):
-        if self.view.qle_url.hasAcceptableInput():
-            self.model.open_web()
-        else:
-            self.view.msg_invalid_url.exec()
+
+        # Opening Chrome Browser
+        self.model.open_web()
+        # Completing first step in the funnel
+        self.model.aggree_terms_cond()
+        # if self.view.qle_url.hasAcceptableInput():
+        #     self.model.open_web()
+        #     # self.model.aggree_terms_cond()
+        # else:
+        #     self.view.msg_invalid_url.exec()
 
     def on_btn_get_url_clicked(self):
         if self.model.driver is not None:
@@ -153,6 +204,12 @@ class TerminBuchenController:
         self.model.stop()
         self.view.btn_stop.setEnabled(False)
         self.view.btn_buchen.setEnabled(True)
+
+# class GetUniqueURL():
+#     def __init__(self):
+#         self.base_url = "https://otv.verwalt-berlin.de/ams/TerminBuchen"
+#         self.link_gener
+
 
 
 if __name__ == '__main__':
